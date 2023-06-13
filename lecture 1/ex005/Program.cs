d9712de22d32a0ecc8a4358ac1538d056d7a30a7
Alena <veter.elena2013@yandex.ru>
2023-06-13 20:42:15 +0300

    ex005

---
+++ lecture 1/ex005/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Write("Введите имя пользователя: ");
+string user = Console.ReadLine();
+
+if (user == "Маша")
+{
+   Console.WriteLine("Это же Машааааа!!!"); 
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(user);
+}
