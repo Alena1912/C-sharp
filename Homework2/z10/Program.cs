2eb615a7c2edd39bba29a670901937343178f9e1
Alena <veter.elena2013@yandex.ru>
2023-06-18 02:51:04 +0300

    z10

---
+++ Homework2/z10/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+void Second(int number)
+    {
+        int dec = (number / 10) % 10;
+        Console.Write("Вторая цифра: " + dec);
+    }
+
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 999 || num < 100)
+{
+    Console.WriteLine("Неправильно введенное число!!!");
+
+}
+else
+{
+    Second(num);
+
+}
