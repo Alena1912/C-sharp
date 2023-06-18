33387d39684cdb8f29d122172f0cd2cdc7e2aa6f
Alena <veter.elena2013@yandex.ru>
2023-06-18 03:12:40 +0300

    z13

---
+++ Homework2/z13/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+void Number3(int number)
+{
+    int three = number % 10;
+    Console.Write("Третья цифра: " + three);
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100)
+{
+    Console.Write(" Третьей цифры нет");
+}
+else
+{
+
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    Number3(num);
+
+}
+
