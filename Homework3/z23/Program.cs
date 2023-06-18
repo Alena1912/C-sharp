e0f2eef416170a08885bfe11892fe4cc45db3e97
Alena <veter.elena2013@yandex.ru>
2023-06-18 22:26:53 +0300

    z23

---
+++ Homework3/z23/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+void Kub(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        double result = Math.Pow(count, 3);
+        Console.Write($"{result:f0} ");
+        count++;
+
+    }
+
+    Console.WriteLine("\b.");
+}
+
+void KubOtr(int num)
+{
+    int count = 1;
+    while (count >= num)
+    {
+        double result = Math.Pow(count, 3);
+        Console.Write($"{result:f0} ");
+        count--;
+
+    }
+
+    Console.WriteLine("\b.");
+}
+
+int number;
+Console.Write("Введите число: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number>0)
+{
+    Kub(number);
+}
+else
+{
+    KubOtr(number);
+}
+
