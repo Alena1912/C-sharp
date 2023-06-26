a74317dea8fca6347f3007f959a1cf82c33d45d4
Alena <veter.elena2013@yandex.ru>
2023-06-27 02:48:15 +0300

    z25

---
+++ Homework4/z25/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+int count = 1;
+int aStepB(int a, int b)
+{
+    for (int i = 0; i < b; i++)
+    {
+        count *= a;
+
+    }
+    return count;
+}
+Console.Write("First number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+count = aStepB(a, b);
+
+Console.WriteLine("A in B = " + count);
