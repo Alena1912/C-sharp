d2ea594113f778d97322fa2160c94d450a6cf0d0
Alena <veter.elena2013@yandex.ru>
2023-06-27 19:50:32 +0300

    z36

---
+++ Homework5/z36/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.Write("The size of our arrey is ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+
+int [] Arr(int size)
+{
+    int [] array = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(-100,101);
+    }
+
+    return array;
+}
+
+int [] arrayPrint(int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write(arr[i] +" ");
+
+    return arr;
+}
+
+void oddCount(int [] arrayEv)
+{
+    int count = 0;
+    for (int i = 0; i < arrayEv.Length; i++)
+    {
+        if (i % 2 == 1)
+            count+=arrayEv[i];
+
+    }
+
+    Console.WriteLine("Summ odd elements of array -> " + count);
+}
+
+int [] arr1 = Arr(size);
+arrayPrint(arr1);
+Console.WriteLine();
+oddCount(arr1);
+
+
+
+
