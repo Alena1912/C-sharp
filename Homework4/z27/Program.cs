fc8ce0227fa68fec894b97146972d42404a99c0e
Alena <veter.elena2013@yandex.ru>
2023-06-27 03:10:00 +0300

    z27

---
+++ Homework4/z27/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+void Sum(int num)
+{
+    int sumNumbers = 0;
+    int rest = num;
+
+    if (rest / 10 != 0)
+    {
+        while (rest != 0)
+        {
+            sumNumbers += (rest % 10);
+            rest /= 10;
+        }
+
+    }
+    else
+    {
+        sumNumbers += (rest % 10);
+    }
+
+    Console.Write("Summ all figures of number : " + sumNumbers);
+}
+
+Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Sum(number);
+
+
