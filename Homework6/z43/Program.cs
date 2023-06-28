9d4b93f6b9e63e45d1fb2121751cace74aa213d2
Alena <veter.elena2013@yandex.ru>
2023-06-28 23:59:09 +0300

    z43

---
+++ Homework6/z43/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * (b2 - b1) / (k1 - k2) + b1;
+
+if (k1 != k2)
+{
+    Console.Write($"Точка пересечения двух прямых: ({x}; {y})");
+}
+else
+{
+    Console.Write("к1 не может ровняться к2");
+}
+
+// Крутила-вертела, но не получилось тут массив втыкнуть)))
+
+
