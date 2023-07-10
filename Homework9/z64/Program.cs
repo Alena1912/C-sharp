48f43ba55233f14398e2ef83bcee5224b2ac2596
Alena <veter.elena2013@yandex.ru>
2023-07-11 02:08:53 +0300

    z64

---
+++ Homework9/z64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.Write("Enter number n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void Recursija(int n)
+{
+    if (n!=0)
+    {
+       Console.Write(n + " ");
+       Recursija(n-1); 
+    }
+    
+}
+
+Recursija(n);
+
